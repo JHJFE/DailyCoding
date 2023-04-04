e78bd88456a39b70f56465a758131c9f89f3d72f
JHJFE <ghwo9611@gmail.com>
2023-04-04 09:07:23 +0900

    2023-04-04

--- programmers/hash/bestalbum.js
+++ programmers/hash/bestalbum.js
@@ -28,4 +28,3 @@ function solution(genres, plays) {
 }
 
 let a = solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500])
-console.log(a)
---
+++ programmers/stack/nosamenumber.js
@@ -0,0 +1,10 @@
+function solution(arr)
+{   
+    var answer = [];
+
+   answer = arr.filter((el,i)=> el !== arr[i-1] )
+
+    return answer;
+}
+let a = solution([1,1,3,3,0,1,1])
+console.log(a)
