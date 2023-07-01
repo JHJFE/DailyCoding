badb6cb9a7a11623497c569f48b75eaf310f2a01
JHJFE <ghwo9611@gmail.com>
2023-07-01 12:52:25 +0900

    2023-07-01

---
+++ practice/63_addSounds.js
@@ -0,0 +1,8 @@
+function solution(absolutes, signs) {
+  return absolutes.reduce((acc, cur, i) => {
+    console.log(acc, cur, i)
+    return signs[i] === true ? acc + cur : acc - cur
+  }, 0)
+}
+let a = solution([1, 2, 3], [false, false, true])
+console.log(a) // 9
