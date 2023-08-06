8a7d3e4adf9bc5372784b77c3e9e49f0a37dbded
JHJFE <ghwo9611@gmail.com>
2023-08-06 23:32:44 +0900

    2023-08-06

---
+++ practice/93.js
@@ -0,0 +1,21 @@
+// 소수찾기
+function solution(n) {
+  var answer = 0
+
+  let primeArr = new Array(n + 1).fill(true)
+  primeArr[0] = false
+  primeArr[1] = false
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (primeArr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        primeArr[j] = false
+      }
+    }
+  }
+  console.log(primeArr)
+  primeArr.map((el) => (el ? answer++ : null))
+  return answer
+}
+let a = solution(16)
+console.log(a)
